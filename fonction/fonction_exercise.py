248f1dfe79899e46d8542e54fa7c472f42121618
akrblt <a.karabulut070@gmail.com>
2024-10-10 10:10:22 +0200

    .

--- fonction/fonction_exercise.py
+++ fonction/fonction_exercise.py
@@ -4,3 +4,13 @@
 # Version : 0.1
 # Purpose : Comprendre les fonctions
 
+
+# pass  # pass Eger bir fonksiyonu tanimladik ama icerigini yazmadigimizda hata vermemesi icin bunu ekliyoruz
+def sayHello(arg_nom):
+    print(f"Bonjour {arg_nom}")
+
+
+
+
+nom=input("Quel est votre nom : ")
+sayHello(nom)
