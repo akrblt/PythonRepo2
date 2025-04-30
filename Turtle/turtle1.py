09f859be1355bc673615afb21f856d5437031c8f
akrblt <a.karabulut070@gmail.com>
2025-04-30 15:13:45 +0200

    feat:turtle

---
+++ Turtle/turtle1.py
@@ -0,0 +1,28 @@
+import turtle
+
+t=turtle.Turtle()
+
+t.pencolor("red")
+t.pensize(3)
+for i in range(100) :
+    t.forward(200)
+    t.right(90)
+    if(i%2==0):
+        t.pencolor("blue")
+        t.forward(200)
+        t.right(135)
+    if(i%3==0):
+        t.pencolor("orange")
+        t.begin_fill()
+    if(i%5==0):
+        t.pencolor("yellow")
+
+
+
+    else:
+
+        t.pencolor("green")
+        t.end_fill()
+
+
+turtle.exitonclick()
